1024c406f3b03024a1025ef26ee34743af6293ee
Daisy Verschoor <157218960+daisythecatgirl@users.noreply.github.com>
2025-02-14 10:31:47 +0100

    update

---
+++ exercise1)14-2-25/exercise1)14-2-25/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercise1_14_2_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
